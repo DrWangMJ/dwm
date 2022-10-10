d00ba4b1cd457029bce8e55d0e5b508da6806bb4
Mingjun Wang <none.wmj@live.com>
2022-10-10 17:13:53 +0800

    add font

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static int smartgaps =
     1; /* 1 means no outer gap when there is only one window */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 0;  /* 0 means bottom bar */
-static const char *fonts[] = {"SauceCodePro Nerd Font Mono:size=18"};
-static const char dmenufont[] = "SauceCodePro Nerd Font Mono:size=18";
+static const char *fonts[] = {"SauceCodePro Nerd Font Mono:size=16", "Symbola:size=16"};
+static const char dmenufont[] = "SauceCodePro Nerd Font Mono:size=16";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
